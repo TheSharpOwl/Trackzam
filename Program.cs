cdbcc3d75e8f9182dffb632e231cbe083a159e6b
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-02-05 12:07:35 +0300

    Made VS project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Trackzam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(RuntimeInformation.FrameworkDescription);
+        }
+    }
+}
