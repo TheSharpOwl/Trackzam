590434dd6f504624d8eb4dd9d1c1be1ce25b262e
Insaf <safinsaft@gmail.com>
2021-02-06 21:43:36 +0300

    Audio data is handled properly and written to database

--- Server/API/parser.py
+++ Server/API/parser.py
@@ -3,17 +3,23 @@
 
 def parse_into_list(file):
     log = []
-    line = file.readline()
-    while line != "":
+    for byte_line in file:
+        line = byte_line.decode("utf-8")
+
         time_and_state = line.split(' ')
         date = time_and_state[0]
         time = time_and_state[1]
         state = time_and_state[2]
-        if state[-1] == '\n':
+        if state[-2:] == '\r\n':
+            state = state[:-2]
+        elif state[-1] == '\n':
             state = state[:-1]
-        node = [date,time,state]
+
+        date_split = date.split("/")
+        date = date_split[2]+'-'+date_split[1]+'-'+date_split[0]
+
+        node = {'date':date,'time':time,'state':state}
         log.append(node)
-        line = file.readline()
     return log
 
 #print(parse_into_list(f))
