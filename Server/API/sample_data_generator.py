fd850720cbc346dce47bf22b6f92cff789702294
GamingJam <GamingJamTv@gmail.com>
2021-02-08 17:03:32 +0300

    Improve sample data generator and add samples for different time periods

--- Server/API/sample_data_generator.py
+++ Server/API/sample_data_generator.py
@@ -4,6 +4,10 @@ import string
 def gen_action_k():
     return random.choice(string.ascii_letters)
 
+def gen_rw():
+    a = ''.join(random.choices(string.ascii_uppercase + string.digits, k=10))
+    return a
+
 #def gen_list_m():
 
 
@@ -15,8 +19,39 @@ def gen_list_k(dur_h, start_h, start_m, start_s, date):
     mins_a = 0 #start_m
     hours_a = 0 #start_h
     out = []
-    while (hours_a*360 + mins_a*60 + secs_a) < dur_h*60*60:
+    while (hours_a*60*60 + mins_a*60 + secs_a) < dur_h*60:
         node = [date, hours, mins, secs, gen_action_k()]
+        out.append(node)
+        secs += 1
+        if secs == 60:
+            secs = 0
+            mins += 1
+            if mins == 60:
+                mins = 0
+                hours += 1
+                if hours == 24:
+                    print("pisos") 
+        secs_a += 1
+        if secs_a == 60:
+            secs_a = 0
+            mins_a += 1
+            if mins_a == 60:
+                mins_a = 0
+                hours_a += 1
+                if hours_a == 24:
+                    print("pisos") 
+    return out
+
+def gen_list_rw(dur_h, start_h, start_m, start_s, date):
+    secs = start_s
+    mins = start_m
+    hours = start_h
+    secs_a = 0 #start_s
+    mins_a = 0 #start_m
+    hours_a = 0 #start_h
+    out = []
+    while (hours_a*60*60 + mins_a*60 + secs_a) < dur_h*60*60:
+        node = [date, hours, mins, secs, gen_rw()]
         out.append(node)
         secs += 1
         if secs == 60:
@@ -54,10 +89,11 @@ def print_doc(name, out):
             s = '0' + str(i[3])
         else:
             s = str(i[3])
+        #a = ''
         a = i[4]
         line = d + ' ' + h + ':' + m + ':' + s + ' ' + a + '\n'
         f.write(line)
     f.close
 
-print_doc('test.txt', gen_list_k(1.5, 12,30,00,'06/02/2021'))
-#print(gen_list_k(0.01, 12,30,00,'06/02/2021'))
+print_doc('test10h.txt', gen_list_rw(10, 12,30,00,'06/02/2021'))
+
