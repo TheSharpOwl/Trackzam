e77d9ed89cbb167166c876d5ac2a5d9bed549223
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-03-06 23:05:33 +0300

    Added a temporary registery storing method.

---
+++ Client/TrackzamClient/UserLogin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackzamClient
+{
+    class UserLogin
+    {
+        // TODO add a hash function and pass the hash to the string
+        // TODO store the username too
+        public void StorePass(string pass)
+        {
+            const string userRoot = "HKEY_CURRENT_USER";
+            const string subkey = "TrackzamData";
+            const string keyName = userRoot + "\\" + subkey;
+            Registry.SetValue(keyName, "PassTest", "let's try a string");
+        }
+
+        public string GetPass()
+        {
+            string ans = (string)Registry.GetValue(keyName, "PassTest", "Failed");
+            return ans;
+        }
+    }
+}
