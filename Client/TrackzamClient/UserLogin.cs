670dfaae6f208aded5bbdb9f68f0800728da8107
TheSharpOwl <ziadxkabakibi@hotmail.com>
2021-03-07 00:40:19 +0300

    Added one way crypting before storing the user password in the registery.

--- Client/TrackzamClient/UserLogin.cs
+++ Client/TrackzamClient/UserLogin.cs
@@ -1,27 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Microsoft.Win32;
+using System.Security.Cryptography;
+using System.Linq;
+
 
 namespace TrackzamClient
 {
-    class UserLogin
+    sealed class UserLogin
     {
-        // TODO add a hash function and pass the hash to the string
+        private const string userRoot = @"HKEY_CURRENT_USER\TrachzamData\";
+
         // TODO store the username too
-        public void StorePass(string pass)
+        public static void StorePass(byte[] hashedPass, string keyName = "UserPass")
         {
-            const string userRoot = "HKEY_CURRENT_USER";
-            const string subkey = "TrackzamData";
-            const string keyName = userRoot + "\\" + subkey;
-            Registry.SetValue(keyName, "PassTest", "let's try a string");
+            string fullKeyName = userRoot + "\\" + keyName;
+
+            if (hashedPass == null)
+            {
+                // TODO handle the not hashing case
+                Console.WriteLine("Failed to hash");
+            }
+
+            Registry.SetValue(fullKeyName, keyName, hashedPass);
         }
 
-        public string GetPass()
+        // TODO make it return the hash not a string
+        public static byte[] GetStoredPass(string keyName)
         {
-            string ans = (string)Registry.GetValue(keyName, "PassTest", "Failed");
-            return ans;
+            byte[] failBytes = Encoding.Default.GetBytes("Failed");
+            byte[] hashedPass = (byte[])Registry.GetValue(userRoot + "\\" + keyName, keyName, failBytes);
+
+            string message = Encoding.Default.GetString(hashedPass);
+            if (message == "Failed")
+            {
+                // TODO handle failed retrieve
+                Console.WriteLine("Failed to retrieve");
+            }
+
+            return hashedPass;
+        }
+
+        private static byte[] Encrypt(string text)
+        {
+            byte[] stringBytes = Encoding.Default.GetBytes(text);
+            // 1 way crypt
+            System.Security.Cryptography.SHA256Managed sha = new System.Security.Cryptography.SHA256Managed();
+            byte[] hashed = sha.ComputeHash(stringBytes);
+            return hashed;
+        }
+
+        //TODO DELETE
+        //THIS Function IS FOR TESTING THE PASSWORD STORING AND COMPARSION
+        public static bool Test()
+        {
+
+            string pass = "bla_bla";
+            byte[] encPass = UserLogin.Encrypt(pass);
+            UserLogin.StorePass(encPass);
+            byte[] TheStoredStuff = UserLogin.GetStoredPass("UserPass");
+            byte[] anotherEnc = UserLogin.Encrypt(pass);
+
+            return anotherEnc.SequenceEqual(encPass);
         }
     }
 }
